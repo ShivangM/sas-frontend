05ce3d4213d4fde938c389e0ec8c4bf2e1fc2792
ShivangM <shivangmishra0824@gmail.com>
2022-04-10 00:48:49 +0530

    charts

--- src/components/Dashboard/Students/AnalyseAttendance/AnalyseAttendance.js
+++ src/components/Dashboard/Students/AnalyseAttendance/AnalyseAttendance.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { PureComponent } from 'react';
+import SubjectsTable from './SubjectsTable'
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
 function AnalyseAttendance() {
-    return (
-        <div className="w-[100%] flex align-middle items-center flex-col">
-            <div className='text-center font-semibold text-3xl py-8'>Analyse Attendance</div>
-        </div>
-    )
-}
+    const data = [
+        { name: 'Present', value: 400 },
+        { name: 'Absent', value: 300 },
+    ];
 
-export default AnalyseAttendance
+    const COLORS = ['#0088FE', '#00C49F'];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+      
+        return (
+          <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+          </text>
+        );
+      };
+
+
+        return (
+            <div className="w-[100%] flex align-middle items-center flex-col">
+                <div className='text-center font-semibold text-3xl py-8'>Analyse Attendance</div>
+                <SubjectsTable />
+
+                <ResponsiveContainer width="100%" height="100%">
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            data={data}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+        )
+    }
+
+    export default AnalyseAttendance
---
+++ src/components/Dashboard/Students/AnalyseAttendance/SubjectsTable.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { useSelector } from "react-redux"
+
+function SubjectsTable() {
+  const thClass =
+    "px-4 py-4 text-left bg-blue-900 text-white text-sm font-medium"
+  const tdClass = "px-4 py-8 border-t border-b border-gray-300 text-sm"
+  const trClass = "border-gray-300 even:bg-gray-300"
+
+  const attendanceData = useSelector(state => state.attendance.attendance)
+
+  return (
+    <table className="w-[90%] table-auto rounded-sm">
+      <thead>
+        <tr>
+          <th className={thClass}>Subject Code</th>
+          <th className={thClass}>Subject Name</th>
+          <th className={thClass}>Faculty</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          attendanceData.length > 0 ?
+            attendanceData.map((value, index) => {
+              return (
+                <tr className={trClass} key={index}>
+                  <td className={tdClass}>{value.subject_code}</td>
+                  <td className={tdClass}>{value.subject_name}</td>
+                  <td className={tdClass}>{value.name}</td>
+                </tr>
+              )
+            })
+            : null
+        }
+      </tbody>
+    </table>
+  )
+}
+
+export default SubjectsTable
