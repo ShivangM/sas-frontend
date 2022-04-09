e924cdbcebe4b7f5487edac68bba19cc71152b9c
ShivangM <shivangmishra0824@gmail.com>
2022-04-09 11:54:40 +0530

    Mobile

--- src/components/Dashboard/SimpleSideNav.js
+++ src/components/Dashboard/SimpleSideNav.js
@@ -17,7 +17,7 @@ function SimpleSideNav() {
 
   let active = "flex justify-between no-underline w-full px-8 py-3 border-l-4 text-sm transition-colors duration-200 ease-in-out hover:text-blue-400 bg-blue-900 border-blue-400 cursor-pointer "
   let inactive = "flex justify-between no-underline w-full px-8 py-3 border-l-2 border-transparent text-sm transition-colors duration-200 ease-in-out hover:text-blue-400 cursor-pointer "
-  
+
   const [activeElement, setActiveElement] = useState(0);
 
   let navClass =
@@ -25,71 +25,74 @@ function SimpleSideNav() {
   if (mobileOpen) navClass += " translate-x-0"
   else navClass += " -translate-x-full lg:translate-x-0"
 
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     localStorage.removeItem('token')
     history("/")
   }
 
-  const userInfo = useSelector(state=> state.userData.userData)
+  const userInfo = useSelector(state => state.userData.userData)
   console.log(userInfo)
 
   return (
-    <div className={navClass}>
+    <div className="">
+
+      <div className={navClass}>
+        <a href="/" className="no-underline block mt-6">
+          <img
+            src="https://mace.edu.in/assets/uploads/notification/logo@2x.png"
+            className="w-40 mx-auto filter invert"
+            alt="logo"
+          />
+        </a>
+        <div className="flex-1 mt-8">
+          <div className="mt-6">
+            <div
+              className={activeElement === 0 ? active : inactive}
+              onClick={() => { setActiveElement(0) }}
+            >
+              <div>
+                <FontAwesomeIcon icon={faHome} className="mr-4" /> View Attendance
+              </div>
+            </div>
+
+            <div
+              className={activeElement === 1 ? active : inactive}
+              onClick={() => { setActiveElement(1) }}
+            >
+              <div>
+                <FontAwesomeIcon icon={faHome} className="mr-4" /> Analyze Attendance
+              </div>
+            </div>
+
+            <div
+              className={activeElement === 2 ? active : inactive}
+              onClick={() => { setActiveElement(2) }}
+            >
+              <div>
+                <FontAwesomeIcon icon={faCog} className="mr-4" /> Account Settings
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex px-8 py-6 items-center">
+          <Avatar name={userInfo ? userInfo.name : "Name"} size="50" round={true} />
+          <div className="flex-1 ml-4">
+            <p className="font-medium leading-none">{userInfo ? userInfo.name : "Name"}</p>
+            <p
+              className="no-underline text-xs text-gray-300 leading-none cursor-pointer mt-1"
+              onClick={handleLogout}
+            >
+              Logout
+            </p>
+          </div>
+        </div>
+      </div>
       <FontAwesomeIcon
         icon={mobileOpen ? faTimes : faBars}
         onClick={() => setMobileOpen(!mobileOpen)}
-        className="absolute right-0 transform translate-x-double top-0 mt-8 text-3xl text-blue-800 cursor-pointer lg:hidden"
+        className="absolute right-10 transform translate-x-double top-0 mt-10 text-3xl text-blue-800 cursor-pointer lg:hidden"
       />
-      <a href="/" className="no-underline block mt-6">
-        <img
-          src="https://mace.edu.in/assets/uploads/notification/logo@2x.png"
-          className="w-40 mx-auto filter invert"
-          alt="logo"
-        />
-      </a>
-      <div className="flex-1 mt-8">
-        <div className="mt-6">
-          <div
-            className= {activeElement===0? active:inactive}
-            onClick={()=>{setActiveElement(0)}}
-          >
-            <div>
-              <FontAwesomeIcon icon={faHome} className="mr-4" /> View Attendance
-            </div>
-          </div>
-
-          <div
-            className= {activeElement===1? active:inactive}
-            onClick={()=>{setActiveElement(1)}}
-          >
-            <div>
-              <FontAwesomeIcon icon={faHome} className="mr-4" /> Analyze Attendance
-            </div>
-          </div>
-   
-          <div
-            className= {activeElement===2? active:inactive}
-            onClick={()=>{setActiveElement(2)}}
-          >
-            <div>
-              <FontAwesomeIcon icon={faCog} className="mr-4" /> Account Settings
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex px-8 py-6 items-center">
-        <Avatar name={userInfo? userInfo.name : "Name"} size="50" round={true}/>
-        <div className="flex-1 ml-4">
-          <p className="font-medium leading-none">{userInfo? userInfo.name : "Name"}</p>
-          <p
-            className="no-underline text-xs text-gray-300 leading-none cursor-pointer mt-1"
-            onClick={handleLogout}
-          >
-            Logout
-          </p>
-        </div>
-      </div>
     </div>
   )
 }
